b1e90f6a1a0bc612bed3fa73a307e37bad028c3f
Georgi Georgiev <Georgi.Georgiev@docuware.com>
2023-02-15 17:54:46 +0200

    Repro project for MemoryCache statistics issue.

---
+++ src/CachingEventSource.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System.Diagnostics.Tracing;
+
+namespace MemCacheCounter
+{
+    [EventSource(Name = "Microsoft-Extensions-Caching-Memory")]
+    internal sealed class CachingEventSource : EventSource
+    {
+        private readonly IMemoryCache _memoryCache;
+        private PollingCounter _cacheHitsCounter;
+
+        public CachingEventSource(IMemoryCache memoryCache) { _memoryCache = memoryCache; }
+        protected override void OnEventCommand(EventCommandEventArgs command)
+        {
+            if (command.Command == EventCommand.Enable)
+            {
+                if (_cacheHitsCounter == null)
+                {
+                    _cacheHitsCounter = new PollingCounter("cache-hits", this, () =>
+                        _memoryCache.GetCurrentStatistics().TotalHits)
+                    {
+                        DisplayName = "Cache hits",
+                    };
+                }
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _cacheHitsCounter.Dispose();
+            _cacheHitsCounter = null;
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MemCacheCounter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
